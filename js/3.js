12942655ab48460b88f38e6397b8df56ba436fba
cbljo <franklll2010@163.com>
2018-01-10 16:51:12 +0800

    structure done

---
+++ js/3.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    var submit = document.getElementById('b1');
+    var del = document.getElementById('b2');
+    submit.onclick = function(){
+        var inputValue = document.getElementById('text').value;
+        if (inputValue == false){
+            alert('请输入您想要发射的弹幕')
+        }
+        else{
+            
+        }
+
+
+    }
+})
