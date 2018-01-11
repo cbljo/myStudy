bceb73a4bad58090a2bdde45cabbd4cf0904e009
cbljo <franklll2010@163.com>
2018-01-11 17:34:30 +0800

    half done

--- js/3.js
+++ js/3.js
@@ -2,15 +2,25 @@ $(document).ready(function () {
 
     var submit = document.getElementById('b1');
     var del = document.getElementById('b2');
+    var danmuArray = new Array();
+    var main = document.getElementById('main');
+    danmuArray.push();
     submit.onclick = function(){
         var inputValue = document.getElementById('text').value;
         if (inputValue == false){
             alert('请输入您想要发射的弹幕')
         }
         else{
-            
+            danmuArray.push(inputValue)
         }
+        launchDanmu()
+    };
+    launchDanmu = function () {
+        var randomIndex = Math.floor(Math.random()*danmuArray.length);
+        var newDiv = document.createElement('div');
+        newDiv.setAttribute('class','transClass')
+        newDiv.innerText = danmuArray[randomIndex];
+        main.appendChild(newDiv);
+    };
 
-
-    }
 })
