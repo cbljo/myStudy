7db23705c6afba6a5ec8cf878b7634e03fc7bf4b
yuwei li <franklll2010@163.com>
2018-01-12 16:21:14 +0800

    done

--- js/3.js
+++ js/3.js
@@ -13,8 +13,9 @@ $(document).ready(function () {
         }
         else{
             danmuArray.push(inputValue)
+            start()
         }
-        start()
+
     };
     launchDanmu = function () {
         var randomIndex = Math.floor(Math.random()*danmuArray.length);
@@ -24,10 +25,11 @@ $(document).ready(function () {
         };
         newDiv.style.color='rgb('+randomColor()+','+randomColor()+','+randomColor()+')';
         newDiv.style.position = 'absolute';
-        newDiv.style.marginRight = '-1000px'
+        //newDiv.style.marginRight = '-1700px';
         newDiv.style.marginTop = Math.random()*380+'px';
         //newDiv.style.transform = 'translate(0px,'+Math.random()*400+'px)';
         newDiv.style.animation = 'trans '+(Math.random()*5+3)+'s linear';
+        newDiv.style.marginLeft = '-900px';
         newDiv.innerText = danmuArray[randomIndex];
         newDiv.style.overflow = 'hidden';
         main.appendChild(newDiv);
@@ -47,10 +49,11 @@ $(document).ready(function () {
 
     };*/
     function start() {
-        var time1 = setInterval('launchDanmu()',3000);
+        time1 = setInterval('launchDanmu()',3000);
         del.onclick = function(){
-            window.clearInterval(time1)
-            $('#main').empty()
+            //window.clearInterval(time1)
+            //$('#main').empty()
+            window.location.reload()
         }
     }
 
