ebcf9b57e229e3c10792aa69c9a28628fae0a008
yuwei li <franklll2010@163.com>
2018-01-17 16:38:37 +0800

    s

--- js/5.js
+++ js/5.js
@@ -1,27 +1,22 @@
-$(document).ready(function () {
-    var b = '北京'
-    var api = 'http://api.avatardata.cn/Weather/Query?key=b101a32b11d74382b8ab1dab6451f270&cityname=' + b;
+$(document).ready(function (data) {
+
+    var b = '北京';
+    var api = 'http://api.avatardata.cn/Weather/Query?key=b101a32b11d74382b8ab1dab6451f270&cityname='+b;
+    window.open(api);
     $.ajax({
-        type:'GET',
+        type:'get',
         url:api,
-        async:true,
-        dataType:"jsonp",    //跨域json请求一定是jsonp
-        jsonp: "callbackparam",    //跨域请求的参数名，默认是callback
-        //jsonpCallback:"successCallback",    //自定义跨域参数值，回调函数名也是一样，默认为jQuery自动生成的字符串
-        data:{"query":"civilnews"},    //请求参数
-
-        beforeSend: function() {
-        },
+        async:false,
+        dataType:"jsonp",
+        jsonp: "callback",
+        jsonpCallback:"success_Callback",
         success: function(data) {
-                 //请求成功处理，和本地回调完全一样
-            alert('ok')
+            console.log(data)
+                 alert(data);
              },
-
-         complete: function() {
-           },
-
-         error: function() {
-            alert('not ok')
-             }
+         error:function () {
+             alert('fail')
+         }
     });
-})
+});
+
