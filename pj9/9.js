7d3352253431b4cbccda30d79def6d0a19e01d87
yuwei li <franklll2010@163.com>
2018-02-27 23:44:20 +0800

    aaa

--- pj9/9.js
+++ pj9/9.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
-    //var position = new Object();
-    alert('ok');
-    //position.x = 0
-    //alert(position.x);
+    var pb = $('.pb');
+    function ini() {
+        for(var i=0;i<pb.length;i++){
+            pb.eq(i).removeClass('cross');
+            pb.eq(i).removeClass('circle')
+        }
+    }
+    var status = 0;
+    var turn = 0;
+    $('#cr').click(function () {
+        ini();
+        status = 0;
+    });
+    $('#ci').click(function () {
+        ini();
+        status = 1;
+    });
+    for(var j=0;j<pb.length;j++){
+        pb.eq(j).click(function () {
+            if(status == 0 && $(this).hasClass('cross')==0 && $(this).hasClass('circle')==0){
+                $(this).addClass('cross');
+                status = 1
+            }
+            else if(status == 1 && $(this).hasClass('cross')==0 && $(this).hasClass('circle')==0){
+                $(this).addClass('circle');
+                status = 0
+            }
+
+        })
+    }
 });
